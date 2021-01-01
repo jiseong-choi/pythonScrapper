f7672fb722265cb4b644e6f98781bc811aaaa09f
newbie webdeveloper <jiseong1951@kakao.com>
2021-01-01 08:26:53 +0000

    add the location

--- indeed.py
+++ indeed.py
@@ -6,45 +6,48 @@ URL = f'https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit={LIMIT}'
 
 
 def extract_indeed_pages():
-  result = requests.get(URL)
-  
-  soup = BeautifulSoup(result.text,"html.parser")
+    result = requests.get(URL)
 
-  pagination = soup.find("div",{"class":"pagination"})
+    soup = BeautifulSoup(result.text, "html.parser")
 
-  links = pagination.find_all('a')
+    pagination = soup.find("div", {"class": "pagination"})
 
-  pages = []
-  
-  for link in links[:-1]:
-    pages.append(int(link.string))
-  
-  max_page = pages[-1]
-  
-  return max_page
+    links = pagination.find_all('a')
+
+    pages = []
+
+    for link in links[:-1]:
+        pages.append(int(link.string))
+
+    max_page = pages[-1]
+
+    return max_page
+
 
 def extract_job(html):
-  title = html.find("h2",{"class":"title"}).find("a")["title"]
-  company = html.find("span",{"class":"company"})
-  company_anchor =  company.find("a")
-  if company_anchor is not None:
-    company = str(company_anchor.string)
-       
-  else:
-    company = str(company.string)
-    company = company.strip()
-  return {'title':title,}
+    title = html.find("h2", {"class": "title"}).find("a")["title"]
+    company = html.find("span", {"class": "company"})
+    company_anchor = company.find("a")
+    if company_anchor is not None:
+        company = str(company_anchor.string)
+
+    else:
+        company = str(company.string)
+        company = company.strip()
+    location = html.find("span", {"class": "location"}).string
+    return {'title': title, 'company': company, 'location': location}
+
 
 def extract_indeed_jobs(last_page):
-  jobs = []
-  #for page in range(last_page):
-  result = requests.get(f"{URL}&start={0*LIMIT}")
-  soup = BeautifulSoup(result.text,"html.parser")
+    jobs = []
+    #for page in range(last_page):
+    result = requests.get(f"{URL}&start={0*LIMIT}")
+    soup = BeautifulSoup(result.text, "html.parser")
 
-  results = soup.find_all("div",{"class":"jobsearch-SerpJobCard"})
-  for result in results:
-    job = extract_job(result)
-  return jobs
-  
-    
-  print(result.status_code)
+    results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
+    for result in results:
+        job = extract_job(result)
+        jobs.append(job)
+    return jobs
+
+    print(result.status_code)
--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from indeed import extract_indeed_pages,extract_indeed_jobs
 
 last_indeed_pages = extract_indeed_pages()
 
-extract_indeed_jobs(last_indeed_pages)
+indeed_jobs = extract_indeed_jobs(last_indeed_pages)
+print(indeed_jobs)
